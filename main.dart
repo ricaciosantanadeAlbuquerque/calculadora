e4b214d9641d08b98b6f8318549c83a06cadcce2
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-04-28 01:22:14 -0300

    tratando exceções

--- main.dart
+++ main.dart
@@ -4,6 +4,8 @@ void main(List<String> args){
 }
 
 void menu(){
+  int valor = 0;
+
   print('========== Menu ==========');
   print('Digite [1] para somar !');
   print('Digite [2] subtrair !');
@@ -11,6 +13,21 @@ void menu(){
   print('Digite [4] dividir !');
   print('Digite [5] para sair !');
   String? entradaOp = stdin.readLineSync();
-  
+
+  if(entradaOp != null){
+    if(entradaOp.isNotEmpty){
+       try{
+         valor = int.parse(entradaOp);
+       }catch(e){
+        throw Exception('$entradaOp, Não é um valor numérico por tanto não pode ser CONVERTIDO !!!');
+       }
+
+    }else{
+      print('ERRO! $entradaOp é nulo ou vazio !! ');
+    }
+  }else{
+      print('ERRO! $entradaOp é nulo ou vazio !! ');
+  }
+
 
 }
