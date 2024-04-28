912a36053869aad5e7a33101f20a830b9aca8023
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-04-28 01:11:11 -0300

    Criando o corpo da função

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
 void main(List<String> args){
   
 }
+
+void menu(){
+  print('========== Menu ==========');
+  print('Digite [1] para somar !');
+  print('Digite [2] subtrair !');
+  print('Digite [3] multiplicar !');
+  print('Digite [4] dividir !');
+  print('Digite [5] para sair !');
+
+
+}
